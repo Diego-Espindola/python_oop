ac34924a784a7b0d27df81180304ba098f8b045b
diego-espindola <diegoespindola9404@gmail.com>
2022-06-29 22:18:14 -0300

    Created class Employee

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 class Employee:
 
+    raise_amt = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -9,8 +11,14 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
 
 emp_1 = Employee('Diego', 'Espindola', 50000)
+print(emp_1.raise_amt)
 emp_2 = Employee('Test', 'Employee', 60000)
 
-print(emp_1.fullname())
+emp_1.apply_raise()
+print(emp_1.pay)
+
